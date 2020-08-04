4373d1878bd560152d2125de1b775006e457b632
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-08-04 16:16:56 -0400

    finished 6 selectors

--- js/index.js
+++ js/index.js
@@ -9,10 +9,22 @@ logoHeader = document.querySelector('.logo-heading').addEventListener('dblclick'
     // event.target.style.color = 'white'
 })
 
+let bodyColorChange = document.querySelector('body').addEventListener('keydown', event => {
+    if(event.keyCode === 70){
+        event.target.style.backgroundColor = '#ADD3E0'
+    }
+})
 
+bodyColorChange = document.querySelector('body').addEventListener('keyup', event => {
+    if(event.keyCode === 70){
+        event.target.style.backgroundColor = 'white'
+    }
+})
 
+let introHeader = document.querySelector('.intro h2').addEventListener('mouseenter', event => {
+    event.target.style.backgroundColor = '#ADD3E0'
+})
 
-
-
-
-
+introHeader = document.querySelector('.intro h2').addEventListener('mouseleave', event => {
+    event.target.style.backgroundColor = 'white'
+})
