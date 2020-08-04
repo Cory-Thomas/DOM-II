efda22408eb9815ac7795f0f12c11baa270eb4fa
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-08-04 15:46:23 -0400

    Finished two listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+let logoHeader = document.querySelector('.logo-heading').addEventListener('click', event => {
+    event.target.style.backgroundColor = '#ADD3E0'
+    event.target.style.color = 'white'
+})
+
+logoHeader = document.querySelector('.logo-heading').addEventListener('dblclick', event => {
+    event.target.style.backgroundColor = 'black'
+    // event.target.style.color = 'white'
+})
+
+
+
+
+
+
+
+
